39ff24a133c92c4cb9a01184c24bd8de12c7eaf2
Justas Vitkauskas <justas.vitkauskas@visma.com>
2022-07-14 16:07:45 +0300

    Removed security from shop module and refactored pom.xml files

--- warehouse/src/main/java/com/visma/warehouse/WarehouseApplication.java
+++ warehouse/src/main/java/com/visma/warehouse/WarehouseApplication.java
@@ -2,10 +2,16 @@ package com.visma.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WarehouseApplication {
+public class WarehouseApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WarehouseApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
     }
