009ae01ddca4a9df649b0895aab4e7b7daedc89d
Justas Vitkauskas <justas.vitkauskas@visma.com>
2022-07-25 12:06:46 +0300

    Created report button

--- shop/src/main/java/com/visma/shop/controllers/ShopController.java
+++ shop/src/main/java/com/visma/shop/controllers/ShopController.java
@@ -3,10 +3,15 @@ package com.visma.shop.controllers;
 import com.visma.shop.services.WarehouseService;
 import com.visma.warehousedto.dto.ProductDto;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Controller
@@ -22,6 +27,17 @@ public class ShopController {
         List<ProductDto> products = warehouseService.getAllProducts();
         modelAndView.addObject("products", products);
         return  modelAndView;
+    }
+
+    @GetMapping("/report/csv")
+    public ResponseEntity<Resource> getReport() {
+        Resource resource = warehouseService.getReport(LocalDateTime.now());
+        String filename = String.format("%s.csv",LocalDateTime.now().truncatedTo(ChronoUnit.HOURS));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(resource);
     }
 
     @PostMapping("/product/buy/{id}")
--- shop/src/main/java/com/visma/shop/services/WarehouseService.java
+++ shop/src/main/java/com/visma/shop/services/WarehouseService.java
@@ -1,11 +1,15 @@
 package com.visma.shop.services;
 
 import com.visma.warehousedto.dto.ProductDto;
-
+import org.springframework.core.io.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface WarehouseService {
 
     List<ProductDto> getAllProducts();
+
     ProductDto buyProduct(long id, int quantity);
+
+    Resource getReport(LocalDateTime date);
 }
